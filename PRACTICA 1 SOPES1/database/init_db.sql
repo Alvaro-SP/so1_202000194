f21da81712f72f169628a48797df0e47ca78ab85
Alvaro-SP <socop2412@gmail.com>
2023-02-03 23:02:15 -0600

    docker

---
+++ PRACTICA 1 SOPES1/database/init_db.sql
@@ -0,0 +1,11 @@
+CREATE DATABASE mydb;
+
+USE mydb;
+
+CREATE TABLE operacion (
+    num1 VARCHAR(255),
+    num2 VARCHAR(255),
+    operator VARCHAR(255),
+    resultado VARCHAR(255),
+    fechayhora VARCHAR(255)
+);
