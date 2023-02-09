55ad142137925832b449bc97e7d55492f7db244b
Alvaro-SP <socop2412@gmail.com>
2023-02-08 23:39:26 -0600

    auto commit

--- PRACTICA 1 SOPES1/sopesprac1/src/App.js
+++ PRACTICA 1 SOPES1/sopesprac1/src/App.js
@@ -6,7 +6,11 @@ import Console from './components/Console';
 
 function App() {
   return (
-    <div className="sopesprac1">
+    <div className="sopesprac1" style={{
+      backgroundImage: `url(${'https://thumbs.gfycat.com/AnchoredSoulfulCapeghostfrog-max-1mb.gif'})`,
+      backgroundSize: 'cover',
+      height: '100vh'
+    }}>
       <Calculator />
       {/* <Console /> */}
     </div>
